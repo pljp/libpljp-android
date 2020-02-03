55c15dd87d86d67243e992e6134c7eb108c62367
k <k@example.com>
2020-02-03 15:54:49 +0900

    SwitchableLiveDataの実装をTransformations.switchMapに変更した。

--- realm/src/main/java/jp/programminglife/libpljp/realm/SwitchableLiveData.kt
+++ realm/src/main/java/jp/programminglife/libpljp/realm/SwitchableLiveData.kt
@@ -1,46 +1,32 @@
 package jp.programminglife.libpljp.realm
 
+import android.os.Looper
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 
 class SwitchableLiveData<T> : LiveData<T>() {
 
-    private var source: LiveData<T>? = null
-    private val observer = Observer<T> { this@SwitchableLiveData.value = it }
+    private val source = MutableLiveData<LiveData<T>>()
+    private val switchable: LiveData<T> = Transformations.switchMap(source) { it }
+    private val observer = Observer<T> { value = it }
 
-
-    fun switch(liveData: LiveData<T>?) {
-        source?.removeObserver(observer)
-        source = liveData
-        liveData?.observeForever(observer)
+    fun switch(liveData: LiveData<T>) {
+        if (Looper.myLooper() == Looper.getMainLooper())
+            source.value = liveData
+        else
+            source.postValue(liveData)
     }
 
 
     override fun onActive() {
-        source?.observeForever(observer)
+        switchable.observeForever(observer)
     }
 
 
     override fun onInactive() {
-        source?.removeObserver(observer)
+        switchable.removeObserver(observer)
     }
-
-//    private val source = MutableLiveData<LiveData<T>>()
-//    private val switchable: LiveData<T> = Transformations.switchMap(source) { it }
-//    private val observer = Observer<T> { value = it }
-//
-//    fun switch(liveData: LiveData<T>) {
-//        source.value = liveData
-//    }
-//
-//
-//    override fun onActive() {
-//        switchable.observeForever(observer)
-//    }
-//
-//
-//    override fun onInactive() {
-//        switchable.removeObserver(observer)
-//    }
 
 }
