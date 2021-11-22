ce0d89508304e18272d252b3a3a6af5ae763038c
k <k@example.com>
2021-11-22 19:51:05 +0900

    UUID生成アプリのノードIDを正しく設定できるようにした。

--- uuidgenerator/src/main/java/jp/programminglife/libpljp/android/uuidgenerator/MainActivity.kt
+++ uuidgenerator/src/main/java/jp/programminglife/libpljp/android/uuidgenerator/MainActivity.kt
@@ -9,28 +9,27 @@ import kotlinx.android.synthetic.main.main_activity_a.*
 class MainActivity : AppCompatActivity() {
     private val log = get(javaClass)
     private lateinit var repository: UuidGenerator.UuidRepository
+    private lateinit var specifiedValueRepository: SpecifiedValueUuidRepository
     private lateinit var uuidGenerator: UuidGenerator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_a)
         repository = UuidGenerator.PreferencesUuidRepository(this)
-        uuidGenerator = UuidGenerator(repository)
+        specifiedValueRepository = SpecifiedValueUuidRepository(repository)
+        uuidGenerator = UuidGenerator(specifiedValueRepository)
 
         a_generate_button.setOnClickListener {
             val n = kotlin.runCatching { a_num_text.text.toString().toInt() }
                     .onFailure { log.d(it, "数のパースエラー") }
                     .getOrDefault(1)
             val nodeIdStr = a_node_id_text.text.toString()
-            val nodeId = if (nodeIdStr.isEmpty()) {
-                val tmpUuid = uuidGenerator.generate(System.currentTimeMillis(), System.nanoTime())
-                tmpUuid.node()
-            }
-            else {
+            specifiedValueRepository.nodeId = if (nodeIdStr.isNotEmpty()) {
                 kotlin.runCatching { nodeIdStr.toLong(16) }
                         .onFailure { log.d(it, "ノードIDのパースエラー") }
                         .getOrDefault(0)
             }
+            else { null }
             val uuidStr = buildString {
                 (0 until n).joinTo(buffer = this, separator = "\n", transform = {
                     uuidGenerator.generate(System.currentTimeMillis(), System.nanoTime()).toString()
@@ -38,5 +37,22 @@ class MainActivity : AppCompatActivity() {
             }
             a_uuid_text.setText(uuidStr)
         }
+    }
+
+
+    private class SpecifiedValueUuidRepository(
+            private val repository: UuidGenerator.UuidRepository
+    ) : UuidGenerator.UuidRepository {
+
+        var nodeId: Long? = null
+
+        override fun loadNodeId(): Long? {
+            return nodeId ?: repository.loadNodeId()
+        }
+
+        override fun saveNodeId(nodeId: Long?) {
+            repository.saveNodeId(nodeId)
+        }
+
     }
 }
